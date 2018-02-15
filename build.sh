79f27721c091548778c96b17619743052736d0b9
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-02-15 20:58:14 +0530

    listing the build settings folder

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ echo "Started db container: $db"
 
 pwd=`pwd`
 echo $pwd
+ls -ltr /var/lib/egov-services/
 ci=`sudo docker run -d -v $pwd:/var/lib/egov-services:rw -w /var/lib/egov-services --net=container:${db} ${ci_image} /bin/bash -c "cd $1; mvn clean verify package -s /var/lib/egov-services/settings.xml"`
 echo "Started ci container to run build: $ci"
 
