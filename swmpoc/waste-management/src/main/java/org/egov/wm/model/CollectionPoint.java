db92839861455007058ca907cb8e7c489af9d4e7
K.Rama Mahidhar Reddy <rammahidhark@gmail.com>
2018-06-20 11:01:47 +0530

    create update search apis added

--- swmpoc/waste-management/src/main/java/org/egov/wm/model/CollectionPoint.java
+++ swmpoc/waste-management/src/main/java/org/egov/wm/model/CollectionPoint.java
@@ -2,6 +2,15 @@ package org.egov.wm.model;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CollectionPoint {
 	private UUID id;
 	
@@ -24,94 +33,6 @@ public class CollectionPoint {
 	private String tenantId;
 	
 	private AuditInfo auditInfo;
-
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
-	public String getBinId() {
-		return binId;
-	}
-
-	public void setBinId(String binId) {
-		this.binId = binId;
-	}
-
-	public Double getLatitude() {
-		return latitude;
-	}
-
-	public void setLatitude(Double latitude) {
-		this.latitude = latitude;
-	}
-
-	public Double getLongitude() {
-		return longitude;
-	}
-
-	public void setLongitude(Double longitude) {
-		this.longitude = longitude;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getWard() {
-		return ward;
-	}
-
-	public void setWard(String ward) {
-		this.ward = ward;
-	}
-
-	public String getStreet() {
-		return street;
-	}
-
-	public void setStreet(String street) {
-		this.street = street;
-	}
-
-	public String getColony() {
-		return colony;
-	}
-
-	public void setColony(String colony) {
-		this.colony = colony;
-	}
-
-	public String getCode() {
-		return code;
-	}
-
-	public void setCode(String code) {
-		this.code = code;
-	}
-
-	public String getTenantId() {
-		return tenantId;
-	}
-
-	public void setTenantId(String tenantId) {
-		this.tenantId = tenantId;
-	}
-
-	public AuditInfo getAuditInfo() {
-		return auditInfo;
-	}
-
-	public void setAuditInfo(AuditInfo auditInfo) {
-		this.auditInfo = auditInfo;
-	}
 	
 	
 }
